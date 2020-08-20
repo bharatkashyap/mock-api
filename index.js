03029b249ef370c0902cfcb6a00c434e1a12a039
Bharat Kashyap <bharatkashyap@outlook.com>
2020-08-21 04:03:56 +0530

    fix: v2 - completely modular no code solution

--- index.js
+++ index.js
@@ -9,61 +9,91 @@ const hello = (req, res, next) => {
     res.send('Hello World!');
 }
 
-const newTrainee = async (req, res, next) => {
-    const trainees = await axios.get('https://api.jsonbin.io/b/5f3a5c31b88c04101cf59111/latest');
-    trainees.data.push(req.body);
-    const updatedTrainees = trainees.data;
-    const updateTrainees = await axios.put('https://api.jsonbin.io/b/5f3a5c31b88c04101cf59111/', updatedTrainees, {
+const jsonBin = {
+    "trainee": "https://api.jsonbin.io/b/5f3ec0ca514ec5112d09fcb6/",
+    "employer": "https://api.jsonbin.io/b/5f3ec430514ec5112d09ff47/",
+    "trainingInstitute": "https://api.jsonbin.io/b/5f3ec8b9514ec5112d0a027d/",
+    "awardingBody": "https://api.jsonbin.io/b/5f3ec9e9993a2e110d322ba9/",
+    "assessmentAgency": "https://api.jsonbin.io/b/5f3eca274d8ce411137c8ae2/",
+    "course": "https://api.jsonbin.io/b/5f3ee674514ec5112d0a18a2/",
+    "batch": "https://api.jsonbin.io/b/5f3eca624d8ce411137c8b09/",
+    "qp": "https://api.jsonbin.io/b/5f3ecc9d4d8ce411137c8c83/",
+    "vacancy": "https://api.jsonbin.io/b/5f3eccbd993a2e110d322d9e/"
+}
+
+const idFields = {
+    "trainee": "trainee_id",
+    "employer": "employer_id",
+    "trainingInstitute": "training_centre_id",
+    "awardingBody": "awarding_body_id",
+    "assessmentAgency": "assessment_agency_id",
+    "course": "classroom_id",
+    "batch": "batch_id",
+    "qp": "qp_code",
+    "vacancy": "job_vacancy_id"
+}
+
+const paths = ['/trainee', '/employer', '/trainingInstitute', '/awardingBody', '/assessmentAgency', '/course', '/batch', '/qp', '/vacancy'];
+
+const create = async (req, res, next) => {
+    const path = req.path.replace('/', '');
+    const list = await axios.get(`${jsonBin[path]}latest`);
+    list.data.push(req.body);
+    const updatedList = list.data;
+    const updateList = await axios.put(jsonBin[path], updatedList, {
         headers: {
             'Content-Type': 'application/json'
         }
     })
-    if(updateTrainees.data.success) res.send(req.body);
+    if(updateList.data.success) res.send(req.body);    
 }
 
-const traineeList = async (req, res, next) => {
-    const trainees = await axios.get('https://api.jsonbin.io/b/5f3a5c31b88c04101cf59111/latest');
-    res.send(trainees.data);
+const getMany = async (req, res, next) => {
+    const path = req.path.replace('/', '');
+    const list = await axios.get(`${jsonBin[path]}latest`);
+    res.send(list.data);
 }
 
-const getTrainee = async (req, res, next) => {
-    const trainees = await axios.get('https://api.jsonbin.io/b/5f3a5c31b88c04101cf59111/latest');
-    let trainee = trainees.data.filter( trainee => trainee.trainee_id === req.params.traineeId);
-    res.send(trainee);
+const getOne = async (req, res, next) => {
+    const path = req.path.replace('/', '').split('/')[0];    
+    const list = await axios.get(`${jsonBin[path]}latest`);    
+    let uniqueItem = list.data.filter( item => item[idFields[path]] === req.params[`${path}Id`]);    
+    res.send(uniqueItem);
 }
 
-const updateTrainees = async (req, res, next) => {
-   const trainees = await axios.get('https://api.jsonbin.io/b/5f3a5c31b88c04101cf59111/latest');
-   req.body.forEach( patchTrainee => {
-       let { trainee_id } = patchTrainee;
-       console.log(trainee_id);
-       if(!trainee_id) res.code(400).send('The trainee ID to update is required.');
+const update = async (req, res, next) => {
+    const path = req.path.replace('/', '');
+    const list = await axios.get(`${jsonBin[path]}latest`);    
+    req.body.forEach( patchItem => {
+       let id = patchItem[idFields[path]];       
+       if(!id) res.status(400).send(`The ${idFields[path]} to update is required.`);
        else {
-            trainees.data.forEach( (trainee, index) => {
-               if(trainee.trainee_id == trainee_id) {
-                   console.log(trainees.data[index]);
-                   console.log(Object.assign(trainee, patchTrainee));
-                   trainees.data[index] = Object.assign(trainee, patchTrainee);
-                   console.log(trainees.data[index]);
+            list.data.forEach( (item, index) => {
+               if(item[idFields[path]] === id) {
+                   list.data[index] = Object.assign(item, patchItem);
                }
            });
        }
    })
-   const updatedTrainees = trainees.data;
-   const updateTrainees = await axios.put('https://api.jsonbin.io/b/5f3a5c31b88c04101cf59111/', updatedTrainees, {
+   const updatedList = list.data;
+   const updateList = await axios.put(jsonBin[path], updatedList, {
         headers: {
             'Content-Type': 'application/json'
         }
     })
-    if(updateTrainees.data.success) res.send(updateTrainees.data.data);
+    if(updateList.data.success) res.send(updateList.data.data);
 }
-  
 
 server.get('/', hello);
-server.post('/trainee', newTrainee)
-server.get('/trainee', traineeList);
-server.patch('/trainee', updateTrainees);
-server.get('/trainee/:traineeId', getTrainee);
+
+paths.forEach( path => {
+    server.post(path, create);
+    server.get(path, getMany);
+    server.patch(path, update);
+    server.get(`${path}/:${path.replace('/', '')}Id`, getOne);
+});
+
+
 
 const port = 8000 || process.env.PORT;
 server.listen(port, () => {
